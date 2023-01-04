b957ac527ad075f01486a01719876bd41bcbf630
Erick Hernandez <yerickk8@gmail.com>
2023-01-04 15:03:17 -0600

    Installed and configured Tailwind Css, Installed Angular Material Componets and Added Sidenav functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'NewsProject';
+  @ViewChild(MatSidenav) sideNav!: MatSidenav
+
+  constructor(private observer: BreakpointObserver, private cdr: ChangeDetectorRef) {
+
+  }
+  ngAfterViewInit(): void {
+    this.sideNav.opened = true;
+    this.observer.observe(['(max-width:800px)'])
+      .subscribe((res) => {
+        if (res?.matches) {
+          this.sideNav.mode = "over";
+          this.sideNav.close();
+        } else {
+          this.sideNav.mode = 'side';
+          this.sideNav.open();
+        }
+      })
+    this.cdr.detectChanges();
+  }
 }
